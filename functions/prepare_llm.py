0f2c7c8749b2ed6825f6fdeb39da5a864f1ae633
dujm <jdu523@outlook.com>
2025-07-31 14:25:06 +0200

    add function

---
+++ functions/prepare_llm.py
@@ -0,0 +1,18 @@
+
+import dotenv
+from langchain_groq import ChatGroq
+
+def prepare_llm(llm_model_id ="llama-3.3-70b-versatile", 
+                temperature=0, 
+                max_tokens=2000):
+    # llm model 
+    dotenv.load_dotenv()
+
+    # Use llm model from Groq 
+    # Get an API here: https://console.groq.com/keys
+    llm=ChatGroq(model_name=llm_model_id,
+                temperature=temperature,
+                max_tokens=max_tokens)
+    return llm
+
+
