9833118badbd5c4b3b74b64a0785d6e35c16bef4
dujm <jdu523@outlook.com>
2025-07-31 22:10:06 +0200

    add function

--- functions/prepare_tag.py
+++ functions/prepare_tag.py
@@ -1,7 +1,7 @@
 import datetime
 
 # generate a tag string 
-def generate_output_string(key_word_no_space):
+def prepare_tag(key_word_no_space):
     # time 
     now=datetime.datetime.now()
     now_str= f"{now:%Y_%m_%d}"
